d35d8bda7d6e819a0eab9679e58bd998fed10a4d
UttamGiri <uttamgiri32@gmail.com>
2021-07-26 20:38:33 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,19 +3,17 @@ export const environment = {
 
   client_id: '********',
   client_secret: '**********',
-
-  loginURL: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/login?client_id=******&response_type=code&scope=openid+profile&redirect_uri=http://localhost:4200/callback',
-
-  redirectURL: 'http://localhost:4200/callback',
-
-  cognitoTokenURL: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/oauth2/token',
-
-  logout: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/logout?' +
-          'client_id=*******&' +
-          'logout_uri=http://localhost:4200/home',
-
-          S3ClientURL: 'https://twfmwqt9t2.execute-api.us-east-1.amazonaws.com/dev',
-  
-          dynamoDBFindURL: 'https://gcwivq0eja.execute-api.us-east-1.amazonaws.com/dev/find'
+        loginURL: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/login?client_id=46s1mf6dm8t88jmfos42ed7noa&response_type=code&scope=openid+profile&redirect_uri=http://localhost:4200/callback',
+      
+        redirectURL: 'http://localhost:4200/callback',
+      
+        cognitoTokenURL: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/oauth2/token',
+      
+        logout: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/logout?' +
+        'client_id=46s1mf6dm8t88jmfos42ed7noa&' +
+        'logout_uri=http://localhost:4200/home',
+      
+                S3ClientURL: 'https://9gjkvybxkk.execute-api.us-east-1.amazonaws.com/dev/find',
+                dynamoDBFindURL: 'https://gcwivq0eja.execute-api.us-east-1.amazonaws.com/dev/find',
 
 }
