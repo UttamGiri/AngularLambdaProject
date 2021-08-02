9b7c58faef293756a3d6bea70106d2d79c7746ee
UttamGiri <uttamgiri32@gmail.com>
2021-08-02 12:06:07 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,21 @@
 export const environment = {
   production: false,
 
-  client_id: '********',
-  client_secret: '**********',
+  client_id: '46s1mf6dm8t88jmfos42ed7noa',
+  client_secret: 'iifrk9dtvddjlipo4tofdgc89drch5gpnqlrjv8rhh53io1f4lr',
 
-  loginURL: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/login?client_id=******&response_type=code&scope=openid+profile&redirect_uri=http://localhost:4200/callback',
+  loginURL: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/login?client_id=46s1mf6dm8t88jmfos42ed7noa&response_type=code&scope=openid+profile&redirect_uri=https://www.dhodekhali.com/callback',
 
-    redirectURL: 'https://www.dhodekhali.com/callback',
+  redirectURL: 'https://www.dhodekhali.com/callback',
 
   cognitoTokenURL: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/oauth2/token',
 
   logout: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/logout?' +
-          'client_id=*******&' +
-          'logout_uri=http://localhost:4200/home',
+          'client_id=46s1mf6dm8t88jmfos42ed7noa&' +
+          'logout_uri=https://www.dhodekhali.com/home',
 
-          S3ClientURL: 'https://twfmwqt9t2.execute-api.us-east-1.amazonaws.com/dev',
-  dynamoDBFindURL: 'https://gcwivq0eja.execute-api.us-east-1.amazonaws.com/dev/find',
+          S3ClientURL: 'https://9gjkvybxkk.execute-api.us-east-1.amazonaws.com/dev/find',
+          dynamoDBFindURL: 'https://gcwivq0eja.execute-api.us-east-1.amazonaws.com/dev/find'
 };
 
 
