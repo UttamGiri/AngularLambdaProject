ed40d6940cf76a6102c56813beb87655e25b1aa9
UttamGiri <uttamgiri32@gmail.com>
2021-08-02 12:01:26 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
    production: false,
-
-  client_id: '********',
-  client_secret: '**********',
+  client_id: '46s1mf6dm8t88jmfos42ed7noa',
+  client_secret: 'iifrk9dtvddjlipo4tofdgc89drch5gpnqlrjv8rhh53io1f4lr',
         loginURL: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/login?client_id=46s1mf6dm8t88jmfos42ed7noa&response_type=code&scope=openid+profile&redirect_uri=http://localhost:4200/callback',
       
          redirectURL: 'https://www.dhodekhali.com/callback',
