445b7cae734a85c13e1a089dbee979067fc8e828
UttamGiri <uttamgiri32@gmail.com>
2021-09-17 19:34:05 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,22 +1,21 @@
 export const environment = {
- production: false,
+  production: false,
 
-  client_id: '*********************',
-  client_secret: '*********************',
+  client_id: '46s1mf6dm8t88jmfos42ed7noa',
+  client_secret: 'iifrk9dtvddjlipo4tofdgc89drch5gpnqlrjv8rhh53io1f4lr',
 
-  loginURL: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/login?client_id=*********************&response_type=code&scope=openid+profile&redirect_uri=https://www.dhodekhali.com/callback',
+  loginURL: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/login?client_id=46s1mf6dm8t88jmfos42ed7noa&response_type=code&scope=openid+profile&redirect_uri=https://www.dhodekhali.com/callback',
 
   redirectURL: 'https://www.dhodekhali.com/callback',
 
   cognitoTokenURL: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/oauth2/token',
 
   logout: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/logout?' +
-          'client_id=*********************&' +
+          'client_id=46s1mf6dm8t88jmfos42ed7noa&' +
           'logout_uri=https://www.dhodekhali.com/home',
 
           S3ClientURL: 'https://9gjkvybxkk.execute-api.us-east-1.amazonaws.com/dev/find',
           dynamoDBFindURL: 'https://gcwivq0eja.execute-api.us-east-1.amazonaws.com/dev/find',
-
           BUILD_NUMBER : 1.1
 };
 
