a1b0794b4fa867f174af6617632e10fddb55efda
UttamGiri <uttamgiri32@gmail.com>
2021-09-17 19:48:19 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,17 @@
 export const environment = {
   production: false,
 
-  client_id: '46s1mf6dm8t88jmfos42ed7noa',
-  client_secret: 'iifrk9dtvddjlipo4tofdgc89drch5gpnqlrjv8rhh53io1f4lr',
+  client_id: '************',
+  client_secret: '************',
 
-  loginURL: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/login?client_id=46s1mf6dm8t88jmfos42ed7noa&response_type=code&scope=openid+profile&redirect_uri=https://www.dhodekhali.com/callback',
+  loginURL: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/login?client_id=************&response_type=code&scope=openid+profile&redirect_uri=https://www.dhodekhali.com/callback',
 
   redirectURL: 'https://www.dhodekhali.com/callback',
 
   cognitoTokenURL: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/oauth2/token',
 
   logout: 'https://uttam-api-auth.auth.us-east-1.amazoncognito.com/logout?' +
-          'client_id=46s1mf6dm8t88jmfos42ed7noa&' +
+          'client_id=************&' +
           'logout_uri=https://www.dhodekhali.com/home',
 
           S3ClientURL: 'https://9gjkvybxkk.execute-api.us-east-1.amazonaws.com/dev/find',
